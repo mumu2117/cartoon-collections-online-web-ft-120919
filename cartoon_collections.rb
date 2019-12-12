31384eb5f642d4043b6a5263adab40e9002c2945
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-12 13:16:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,4 +15,5 @@ end
 def find_the_cheese(cheeses)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.find { |cheese| cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"}
 end
